1b556ee7230b0cdebda395b1c736d9e44841a1ce
BlackMonkcr <101078390+BlackMonkcr@users.noreply.github.com>
2023-10-16 17:41:43 -0500

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/Application/PersonController.java
@@ -0,0 +1,27 @@
+package com.example.demo.Application;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+@RestController
+@RequestMapping("/groups")
+public class GroupController {
+
+    @Autowired
+    private GroupRepository groupRepository;
+    
+    @GetMapping
+    public List<Group> getAllGroups() {
+        return groupRepository.getAllGroups();
+    }
+
+    
+    @GetMapping("/{groupId}")
+    public Group getGroupById(@PathVariable int groupId) {
+        return groupRepository.getGroupById(groupId);
+    }
+
+    
+
+}
